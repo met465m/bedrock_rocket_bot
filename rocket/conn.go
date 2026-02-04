30509f16fb2a729021979f0baa022abf08319e1d
met465m <dolgoshevaanutka@gmail.com>
2026-02-04 07:14:01 +0300

    обновление

--- rocket/conn.go
+++ rocket/conn.go
@@ -1,1 +1,58 @@
+// rocket/conn.go
+package rocket
+
+import (
+	"net"
+	"time"
+)
+
+type Conn struct {
+	UDP      *net.UDPConn
+	Addr     *net.UDPAddr
+	MTU      int
+	Closed   bool
+	CloseCh  chan struct{}
+}
+
+func Dial(addr string) (*Conn, error) {
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Conn{
+		UDP:     conn,
+		Addr:    udpAddr,
+		MTU:     1400,
+		CloseCh: make(chan struct{}),
+	}
+
+	return c, nil
+}
+
+func (c *Conn) Write(b []byte) error {
+	if c.Closed {
+		return nil
+	}
+	_, err := c.UDP.Write(b)
+	return err
+}
+
+func (c *Conn) Read(b []byte) (int, error) {
+	return c.UDP.Read(b)
+}
+
+func (c *Conn) Close() {
+	if c.Closed {
+		return
+	}
+	c.Closed = true
+	close(c.CloseCh)
+	_ = c.UDP.Close()
+}
 
