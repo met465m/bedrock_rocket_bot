1e43bedef194a86c093718da41a73d854727d708
met465m <dolgoshevaanutka@gmail.com>
2026-02-05 06:44:05 +0300

    .

--- rocket/util/buffer.go
+++ rocket/util/buffer.go
@@ -1,1 +1,19 @@
+package bedrock_rocket_bot
+
+import "bytes"
+
+// Buffer — упрощённый буфер
+type Buffer struct {
+	Data []byte
+}
+
+// NewBuffer создаёт буфер
+func NewBuffer() *Buffer {
+	return &Buffer{Data: make([]byte, 0)}
+}
+
+// Write добавляет данные
+func (b *Buffer) Write(data []byte) {
+	b.Data = append(b.Data, data...)
+}
 
