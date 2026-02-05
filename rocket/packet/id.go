b3bc91a14df5cc98d63ef297f909d3f124e1f278
met465m <dolgoshevaanutka@gmail.com>
2026-02-05 06:40:13 +0300

    .

--- rocket/packet/id.go
+++ rocket/packet/id.go
@@ -1,1 +1,15 @@
+package bedrock_rocket_bot
+
+const (
+	// RakNet
+	OpenConnectionRequest1 = 0x05
+	OpenConnectionReply1   = 0x06
+
+	// Minecraft
+	LoginPacketID   = 0x01
+	PlayStatusID    = 0x02
+	MovePlayerID    = 0x02
+	TextPacketID    = 0x03
+	DisconnectID    = 0xFF
+)
 
